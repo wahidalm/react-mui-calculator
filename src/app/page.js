a36ca24e5d38e5c22e2dd724f25484993df7ebc5
wahidalm <wahiduzalam@gmail.com>
2025-09-18 22:43:49 +0530

    Initial commit: Set up functional calculator with MUI

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,145 @@
-import Image from "next/image";
+'use client'
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import Paper from "@mui/material/Paper";
+import BackspaceIcon from '@mui/icons-material/Backspace';
+
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [result, setResult] = useState("0");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const handleButtonClick = value => {
+    let newResult = result + value;
+    if (newResult.startsWith("0") && newResult.length > 1) {
+        setResult(newResult.substring(1));
+    } else {
+      setResult(newResult);
+}
+  }
+  return (
+<Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: '100vh' }}
+    >
+
+        <Stack
+          direction="column"
+          spacing={2}
+        >
+          <Paper
+            elevation={3}
+            sx={{
+              padding: 2,
+              textAlign: 'right',
+              backgroundColor: '#696969',
+              color: '#f000000',
+              overflow: 'hidden',
+              minHeight: 50,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+            }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            <Box sx={{justifyContent: "flex-end", width: '100%'}}>
+              <Typography variant="h4" align="right" fontFamily= 'monospace' fontWeight='bold' > {result} </Typography>
+            </Box>
+           </Paper>
+          <Stack direction="row" spacing={2}>
+            <Button 
+            variant="outlined" 
+            sx={{backgroundColor: '#ff5810', color: '#000', '&:hover': {
+            backgroundColor: '#f57c00', },}}
+            onClick={() => setResult('0')}>AC</Button>
+            <Button variant="outlined" sx={{backgroundColor: '#ff9810', color: '#000', '&:hover': {
+            backgroundColor: '#f57c00', },}}
+                        onClick={() => {
+              setResult(prevResult => prevResult + "/");
+            }}
+            >/</Button>
+            <Button variant="outlined" sx={{backgroundColor: '#ff9800', color: '#000', '&:hover': {
+            backgroundColor: '#f57c00', },}}
+                        onClick={() => {
+              setResult(prevResult => prevResult + "*");
+            }}
+            >*</Button>
+            <Button variant="outlined" sx={{backgroundColor: '#ff9800', color: '#000', '&:hover': {
+            backgroundColor: '#f57c00', },}}
+                        onClick={() => {
+              setResult(prevResult => prevResult + "C");
+            }}
+            >C</Button>
+          </Stack>
+
+          <Stack direction="row" spacing={2} sx={{justifyContent: "center"}}>
+            <Button variant="outlined"
+            onClick={() => handleButtonClick('1')}
+            >1</Button>
+            <Button variant="outlined"
+              onClick={() => handleButtonClick('2')}
+            >2</Button>
+            <Button variant="outlined"
+             onClick={() => handleButtonClick('3')}
+            >3</Button>
+            <Button variant="outlined" sx={{backgroundColor: '#ff9800', color: '#000', '&:hover': {
+            backgroundColor: '#f57c00', },}}>%</Button>
+          </Stack>
+
+          <Stack direction="row" spacing={2} sx={{justifyContent: "center"}}>
+            <Button variant="outlined"
+             onClick={() => handleButtonClick('4')}
+            >4</Button>
+            <Button variant="outlined"
+             onClick={() => handleButtonClick('5')}
+            >5</Button>
+            <Button variant="outlined"
+            onClick={() => handleButtonClick('6')}
+            >6</Button>
+            <Button variant="outlined" sx={{backgroundColor: '#ff9800', color: '#000', '&:hover': {
+            backgroundColor: '#f57c00', },}}
+                        onClick={() => {
+              setResult(prevResult => prevResult + "-");
+            }}
+            >-</Button>
+          </Stack>
+
+          <Stack direction="row" spacing={2} sx={{justifyContent: "center"}}>
+            <Button variant="outlined"
+              onClick={() => handleButtonClick('7')}
+            >7</Button>
+            <Button variant="outlined"
+               onClick={() => handleButtonClick('8')}
+            >8</Button>
+            <Button variant="outlined"
+               onClick={() => handleButtonClick('9')}
+            >9</Button>
+            <Button variant="outlined" sx={{backgroundColor: '#ff9800', color: '#000', '&:hover': {
+            backgroundColor: '#f57c00', },}}
+                        onClick={() => {
+              setResult(prevResult => prevResult + "+");
+            }}
+            >+</Button>
+          </Stack>
+
+          <Stack direction="row" spacing={2} sx={{justifyContent: "center"}}>
+            <Button variant="outlined"
+               onClick={() => handleButtonClick('0')}
+            >0</Button>
+            <Button variant="outlined"
+               onClick={() => handleButtonClick('.')}
+            >.</Button>
+            <Button variant="outlined" sx={{backgroundColor: '#ff9800', color: '#000', '&:hover': {
+            backgroundColor: '#f57c00', },}}
+            onClick={() => handleButtonClick('=')}>DEL</Button>
+            <Button variant="outlined" sx={{backgroundColor: '#ff9800', color: '#000', '&:hover': {
+            backgroundColor: '#f57c00', },}}>=</Button>
+          </Stack>
+
+          </Stack>
+</Box>
   );
 }
