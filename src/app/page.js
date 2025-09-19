848e47becb17d95768e0ba1420f162bfb08a8766
wahidalm <wahiduzalam@gmail.com>
2025-09-19 16:01:11 +0530

    Make backspace work

--- src/app/page.js
+++ src/app/page.js
@@ -1,145 +1,231 @@
-'use client'
-import { useState } from 'react';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
+"use client";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
-import BackspaceIcon from '@mui/icons-material/Backspace';
-
+import IconButton from "@mui/material/IconButton";
+import BackspaceIcon from "@mui/icons-material/Backspace";
 
 export default function Home() {
   const [result, setResult] = useState("0");
 
-  const handleButtonClick = value => {
+  const handleButtonClick = (value) => {
     let newResult = result + value;
     if (newResult.startsWith("0") && newResult.length > 1) {
-        setResult(newResult.substring(1));
+      setResult(newResult.substring(1));
     } else {
       setResult(newResult);
-}
+    }
   }
+
+  const handleBackspace = () => {
+    let newResult = result;
+    if (newResult.length>1) {
+      setResult (newResult.slice(0, -1));
+  } else {
+     setResult (0);
+  }
+}
+
   return (
-<Box
+    <Box
       display="flex"
       alignItems="center"
       justifyContent="center"
-      sx={{ minHeight: '100vh' }}
+      sx={{ minHeight: "100vh" }}
     >
+      <Stack direction="column" spacing={2}>
+        <Paper
+          elevation={3}
+          sx={{
+            padding: 2,
+            textAlign: "right",
+            backgroundColor: "#696969",
+            color: "#f000000",
+            overflow: "hidden",
+            minHeight: 50,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+          }}
+        >
+          <Box sx={{ justifyContent: "flex-end", width: "100%" }}>
+            <Typography
+              variant="h4"
+              align="right"
+              fontFamily="monospace"
+              fontWeight="bold"
+            >
+              {" "}
+              {result}{" "}
+            </Typography>
+          </Box>
+        </Paper>
+        <Stack direction="row" spacing={2}>
+          <Button
+            variant="outlined"
+            sx={{
+              backgroundColor: "#ff5810",
+              color: "#000",
+              "&:hover": {
+                backgroundColor: "#f57c00",
+              },
+            }}
+            onClick={() => setResult("0")}
+          >
+            AC
+          </Button>
+          <Button
+            variant="outlined"
+            sx={{
+              backgroundColor: "#ff9810",
+              color: "#000",
+              "&:hover": {
+                backgroundColor: "#f57c00",
+              },
+            }}
+            onClick={() => setResult("/")}
+          >
+            /
+          </Button>
+          <Button
+            variant="outlined"
+            sx={{
+              backgroundColor: "#ff9800",
+              color: "#000",
+              "&:hover": {
+                backgroundColor: "#f57c00",
+              },
+            }}
+            onClick={() => setResult("*")}
+          >
+            *
+          </Button>
+          <Button
+            variant="outlined"
+            sx={{
+              backgroundColor: "#ff9800",
+              color: "#000",
+              "&:hover": {
+                backgroundColor: "#f57c00",
+              },
+            }}
+            onClick={() => setResult("C")}
+          >
+            C
+          </Button>
+        </Stack>
+
+        <Stack direction="row" spacing={2} sx={{ justifyContent: "center" }}>
+          <Button variant="outlined" onClick={() => handleButtonClick("1")}>
+            1
+          </Button>
+          <Button variant="outlined" onClick={() => handleButtonClick("2")}>
+            2
+          </Button>
+          <Button variant="outlined" onClick={() => handleButtonClick("3")}>
+            3
+          </Button>
+          <Button
+            variant="outlined"
+            sx={{
+              backgroundColor: "#ff9800",
+              color: "#000",
+              "&:hover": {
+                backgroundColor: "#f57c00",
+              },
+            }}
+              onClick={() => setResult("%")}
+          >
+            %
+          </Button>
+        </Stack>
+
+        <Stack direction="row" spacing={2} sx={{ justifyContent: "center" }}>
+          <Button variant="outlined" onClick={() => handleButtonClick("4")}>
+            4
+          </Button>
+          <Button variant="outlined" onClick={() => handleButtonClick("5")}>
+            5
+          </Button>
+          <Button variant="outlined" onClick={() => handleButtonClick("6")}>
+            6
+          </Button>
+          <Button
+            variant="outlined"
+            sx={{
+              backgroundColor: "#ff9800",
+              color: "#000",
+              "&:hover": {
+                backgroundColor: "#f57c00",
+              },
+            }}
+            onClick={() => setResult("-")}
+          >-
+          </Button>
+        </Stack>
 
         <Stack
-          direction="column"
+          direction="row"
           spacing={2}
+          sx={{ justifyContent: "space-between" }}
         >
-          <Paper
-            elevation={3}
+          <Button variant="outlined" onClick={() => handleButtonClick("7")}>
+            7
+          </Button>
+          <Button variant="outlined" onClick={() => handleButtonClick("8")}>
+            8
+          </Button>
+          <Button variant="outlined" onClick={() => handleButtonClick("9")}>
+            9
+          </Button>
+          <Button
+            variant="outlined"
             sx={{
-              padding: 2,
-              textAlign: 'right',
-              backgroundColor: '#696969',
-              color: '#f000000',
-              overflow: 'hidden',
-              minHeight: 50,
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'flex-end',
+              backgroundColor: "#ff9800",
+              color: "#000",
+              "&:hover": {
+                backgroundColor: "#f57c00",
+              },
             }}
+            onClick={() => handleButtonClick("+")}>
+            +
+          </Button>
+        </Stack>
+
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{ justifyContent: "space-between" }}
+        >
+          <Button variant="outlined" onClick={() => handleButtonClick("0")}>
+            0
+          </Button>
+          <Button variant="outlined" onClick={() => handleButtonClick(".")}>
+            .
+          </Button>
+
+          <IconButton color="primary" onClick={handleBackspace} >
+            <BackspaceIcon />
+          </IconButton>
+
+          <Button
+            variant="outlined"
+            sx={{
+              backgroundColor: "#ff9800",
+              color: "#000",
+              "&:hover": {
+                backgroundColor: "#f57c00",
+              },
+            }}
+
           >
-            <Box sx={{justifyContent: "flex-end", width: '100%'}}>
-              <Typography variant="h4" align="right" fontFamily= 'monospace' fontWeight='bold' > {result} </Typography>
-            </Box>
-           </Paper>
-          <Stack direction="row" spacing={2}>
-            <Button 
-            variant="outlined" 
-            sx={{backgroundColor: '#ff5810', color: '#000', '&:hover': {
-            backgroundColor: '#f57c00', },}}
-            onClick={() => setResult('0')}>AC</Button>
-            <Button variant="outlined" sx={{backgroundColor: '#ff9810', color: '#000', '&:hover': {
-            backgroundColor: '#f57c00', },}}
-                        onClick={() => {
-              setResult(prevResult => prevResult + "/");
-            }}
-            >/</Button>
-            <Button variant="outlined" sx={{backgroundColor: '#ff9800', color: '#000', '&:hover': {
-            backgroundColor: '#f57c00', },}}
-                        onClick={() => {
-              setResult(prevResult => prevResult + "*");
-            }}
-            >*</Button>
-            <Button variant="outlined" sx={{backgroundColor: '#ff9800', color: '#000', '&:hover': {
-            backgroundColor: '#f57c00', },}}
-                        onClick={() => {
-              setResult(prevResult => prevResult + "C");
-            }}
-            >C</Button>
-          </Stack>
-
-          <Stack direction="row" spacing={2} sx={{justifyContent: "center"}}>
-            <Button variant="outlined"
-            onClick={() => handleButtonClick('1')}
-            >1</Button>
-            <Button variant="outlined"
-              onClick={() => handleButtonClick('2')}
-            >2</Button>
-            <Button variant="outlined"
-             onClick={() => handleButtonClick('3')}
-            >3</Button>
-            <Button variant="outlined" sx={{backgroundColor: '#ff9800', color: '#000', '&:hover': {
-            backgroundColor: '#f57c00', },}}>%</Button>
-          </Stack>
-
-          <Stack direction="row" spacing={2} sx={{justifyContent: "center"}}>
-            <Button variant="outlined"
-             onClick={() => handleButtonClick('4')}
-            >4</Button>
-            <Button variant="outlined"
-             onClick={() => handleButtonClick('5')}
-            >5</Button>
-            <Button variant="outlined"
-            onClick={() => handleButtonClick('6')}
-            >6</Button>
-            <Button variant="outlined" sx={{backgroundColor: '#ff9800', color: '#000', '&:hover': {
-            backgroundColor: '#f57c00', },}}
-                        onClick={() => {
-              setResult(prevResult => prevResult + "-");
-            }}
-            >-</Button>
-          </Stack>
-
-          <Stack direction="row" spacing={2} sx={{justifyContent: "center"}}>
-            <Button variant="outlined"
-              onClick={() => handleButtonClick('7')}
-            >7</Button>
-            <Button variant="outlined"
-               onClick={() => handleButtonClick('8')}
-            >8</Button>
-            <Button variant="outlined"
-               onClick={() => handleButtonClick('9')}
-            >9</Button>
-            <Button variant="outlined" sx={{backgroundColor: '#ff9800', color: '#000', '&:hover': {
-            backgroundColor: '#f57c00', },}}
-                        onClick={() => {
-              setResult(prevResult => prevResult + "+");
-            }}
-            >+</Button>
-          </Stack>
-
-          <Stack direction="row" spacing={2} sx={{justifyContent: "center"}}>
-            <Button variant="outlined"
-               onClick={() => handleButtonClick('0')}
-            >0</Button>
-            <Button variant="outlined"
-               onClick={() => handleButtonClick('.')}
-            >.</Button>
-            <Button variant="outlined" sx={{backgroundColor: '#ff9800', color: '#000', '&:hover': {
-            backgroundColor: '#f57c00', },}}
-            onClick={() => handleButtonClick('=')}>DEL</Button>
-            <Button variant="outlined" sx={{backgroundColor: '#ff9800', color: '#000', '&:hover': {
-            backgroundColor: '#f57c00', },}}>=</Button>
-          </Stack>
-
-          </Stack>
-</Box>
+            =
+          </Button>
+        </Stack>
+      </Stack>
+    </Box>
   );
 }
